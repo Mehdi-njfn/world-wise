38f0b5dd44b691c026315293bb144c03a3e4698b
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-06-16 21:25:55 +0330

    city item

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import styles from "../starter/components/CityItem.module.css";
+import { useCities } from "../context/CitiesCtx";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    // weekday: "long",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  const { cityName, emoji, date, id, position } = city;
+  const { currentCity } = useCities();
+  return (
+    <li>
+      <Link
+        to={`cities/${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles['cityItem--active'] : ""
+        }`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
+    </li>
+  );
+}
+
+export default CityItem;
