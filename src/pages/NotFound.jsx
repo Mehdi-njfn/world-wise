c3424e2da5f91e2f3ba47503eefb7fc1791eff2a
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-06-16 21:28:28 +0330

    not found

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom"
+
+function NotFound() {
+  return (
+    <div>
+      <NavLink to='/'>Go Back</NavLink>
+      <h2>Not Founded</h2>
+    </div>
+  )
+}
+
+export default NotFound
