22a794594c1e075e4ba64d21dcb5da9aa3ae66a5
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-06-21 23:09:28 +0330

    map up to date

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,28 +1,49 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "../starter/components/Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import { useEffect, useState } from "react";
 import { useCities } from "../context/CitiesCtx";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 function Map() {
   const { cities } = useCities();
-
   const [mapPosition, setMapPostion] = useState([40, 0]);
-
-  const [searchParams] = useSearchParams();
-
-  const mapLat = searchParams?.get("lat");
-  const mapLng = searchParams?.get("lng");
-
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+  const [mapLat ,mapLng] = useUrlPosition()
+  
   useEffect(
     function () {
       if (mapLat && mapLng) setMapPostion([mapLat, mapLng]);
     },
     [mapLat, mapLng]
   );
-
+  // useEffect(
+  //    ()=> {
+  //     if (geolocationPosition)
+  //       setMapPostion([geolocationPosition.lat, geolocationPosition.lng]);
+  //   },
+  //   [geolocationPosition]
+  // );
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition.lat && (
+        <Button type="position" onclick={() => getPosition()}>
+          {isLoadingPosition ? "Loading..." : "Use Your Position"}
+        </Button>
+      )}
       <MapContainer
         className={styles.map}
         center={mapPosition}
@@ -33,7 +54,7 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        {cities.map((c) => (
+        {cities?.map((c) => (
           <Marker position={[c.position.lat, c.position.lng]} key={c.id}>
             <Popup>{c.cityName}</Popup>
           </Marker>
@@ -49,13 +70,12 @@ function ChangeCenter({ position }) {
   map.setView(position);
   return null;
 }
-function DetectClick(){
-  const navigate = useNavigate()
+function DetectClick() {
+  const navigate = useNavigate();
   useMapEvents({
-    click: e=>{
-      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
-
-    }
-  })
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 export default Map;
