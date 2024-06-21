fceb7bd2b9212e4e43d7092d0ee83c32d3bfd04a
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-06-21 23:10:09 +0330

    commit

--- src/context/AuthCtx.js
+++ src/context/AuthCtx.js
@@ -1,64 +1,63 @@
-import { createContext, useContext, useReducer } from "react";
+// import { createContext, useContext, useReducer } from "react";
 
-const AuthContext = createContext();
-const initialState = {
-  user: null,
-  isAuthenticated: false,
-};
+// const AuthCtx = createContext();
+// const initialState = {
+//   user: null,
+//   isAuthenticated: false,
+// };
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
+// const FAKE_USER = {
+//   name: "Jack",
+//   email: "jack@example.com",
+//   password: "qwerty",
+//   avatar: "https://i.pravatar.cc/100?u=zz",
+// };
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "login":
-      return {
-        ...state,
-        user: action.payload,
-        isAuthenticated: true,
-      };
-    case "logout":
-      return {
-        ...state,
-        user: null,
-        isAuthenticated: false,
-      };
-    default:
-      throw new Error("action is unknown type ");
-  }
-}
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "login":
+//       return {
+//         ...state,
+//         user: action.payload,
+//         isAuthenticated: true,
+//       };
+//     case "logout":
+//       return {
+//         ...state,
+//         user: null,
+//         isAuthenticated: false,
+//       };
+//     default:
+//       throw new Error("action is unknown type ");
+//   }
+// }
 
-function AuthProvider({ children }) {
-  const [{ user, isAuthenticated }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-  function login(email, password) {
-    if (email === FAKE_USER.email && password === FAKE_USER.password)
-      dispatch({ type: "login", payload: FAKE_USER });
-  }
+// function AuthProvider({ children }) {
+//   const [{ user, isAuthenticated }, dispatch] = useReducer(
+//     reducer,
+//     initialState
+//   );
+//   function login(email, password) {
+//     if (email === FAKE_USER.email && password === FAKE_USER.password)
+//       dispatch({ type: "login", payload: FAKE_USER });
+//   }
 
-  function logout() {
-    dispatch({ type: "logout" });
-  }
+//   function logout() {
+//     dispatch({ type: "logout" });
+//   }
 
-  return (
-    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-}
-function useAuth() {
-  const context = useContext(AuthContext);
+//   return (
+//     <AuthCtx.Provider value={{ user, isAuthenticated, login, logout }}>
+//       {children}
+//     </AuthCtx.Provider>
+//   );
+// }
+// function useAuth() {
+//   const context = useContext(AuthCtx);
 
-  if (context === undefined)
-    throw new Error("AuthContext was be used outside AuthProvider");
-  
-  return context;
-}
+//   if (context === undefined)
+//     throw new Error("CitiesContext was be used outside of CitiesProvider!");
+//   return context;
+// }
 
-// export { useAuth,AuthProvider, AuthContext };
+// export { useAuth,AuthProvider, AuthCtx };
