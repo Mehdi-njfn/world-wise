81540f9733ba5220f6d616542559fe4e4191f34b
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-06-21 23:08:34 +0330

    city item up to date

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -12,19 +12,26 @@ const formatDate = (date) =>
 
 function CityItem({ city }) {
   const { cityName, emoji, date, id, position } = city;
-  const { currentCity } = useCities();
+  const { currentCity ,deleteCity} = useCities();
+
+  function handleDelete(e) {
+    e.preventDefault();
+    deleteCity(id)
+  }
   return (
     <li>
       <Link
         to={`cities/${id}?lat=${position.lat}&lng=${position.lng}`}
         className={`${styles.cityItem} ${
-          id === currentCity.id ? styles['cityItem--active'] : ""
+          id === currentCity.id ? styles["cityItem--active"] : ""
         }`}
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button onClick={handleDelete} className={styles.deleteBtn}>
+          &times;
+        </button>
       </Link>
     </li>
   );
